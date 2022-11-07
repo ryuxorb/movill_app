3a6604b74d09a9847e239dd27d625a7370cb9713
Taegyu Ryu <xorb1017@naver.com>
2022-11-07 17:32:05 +0900

    검색, 알림, 마이페이지 페이지 레이아웃 추가

---
+++ lib/src/pages/mypage.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:movill/src/components/avatar_widget.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  Widget myAvatar() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          AvatarWidget(
+              type: AvatarType.Type1,
+              thumbPath:
+                  'https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/cEmV/image/waD2sHkLtNV__uKphcoyhj3f_OE.jpg')
+        ],
+      ),
+    );
+  }
+
+  Widget information() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            myAvatar(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'OOO',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  '+82 10-9454-4394',
+                  style: TextStyle(color: Colors.black54, fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 94,
+            ),
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.add_circle),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget menu() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 250,
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.access_time_outlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Text(
+                    '공지사항',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 236,
+                  ),
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.add_circle),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.add_chart),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Text(
+                    '사용내역',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 236,
+                  ),
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.add_circle),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.add_chart),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Text(
+                    '차량관리',
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 236,
+                  ),
+                  Container(
+                    child: IconButton(
+                      icon: Icon(Icons.add_circle),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          '내 정보',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black87),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            information(),
+            menu(),
+          ],
+        ),
+      ),
+    );
+  }
+}
